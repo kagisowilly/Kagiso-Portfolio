4408c8804e3a4f814cea85d9908d0c19a8d40290
kagisowilly <mphayiw@gmail.com>
2022-05-16 22:02:01 +0200

    changes

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,44 +1,39 @@
 <template>
   <!-- RESUME -->
 
-  <section
-    class="education-work"
-    id="education"
-  >
-            <h1 class="text-center display-6 green fw-bold subtitlee mb-5">
-            <u>Resume</u>
-          </h1>
-          <h3 class="text-center pt-5">Education & Experience</h3>
+  <section class="education-work" id="education">
+    <h1 class="text-center display-6 green fw-bold subtitlee mb-5">
+      <u>Resume</u>
+    </h1>
+    <h3 class="text-center edu-resu pt-5">Education & Experience</h3>
     <div class="container mb-5">
-      <div class="row  justify-content-center" v-if="resumes">
+      <div class="row justify-content-center" v-if="resumes">
         <div class="col-lg-10 text-black">
-
           <ul class="timeline">
             <li class="slide" v-for="resume of resumes" :key="resume.title">
               <div class="resu box-shadow">
                 <div class="p-2">
-                  <h3 class="green">
-                    <i class="bi bi-briefcase-fill"></i> {{resume.title}}
+                  <h3 class="green title">
+                    <i class="bi bi-calendar-fill"></i>
+                    {{ resume.title }}
                   </h3>
                   <div style="margin-left: 30px">
-                    <h6 class="text-muted">{{resume.period}}</h6>
-                    <h5>{{resume.institution}}</h5>
-                    <p>
-                        {{resume.description}}
+                    <h6 class="text-muted period">{{ resume.period }}</h6>
+                    <h5>{{ resume.institution }}</h5>
+                    <p class="description">
+                      {{ resume.description }}
                     </p>
                   </div>
                 </div>
               </div>
             </li>
-            
+
             <br />
           </ul>
         </div>
       </div>
       <div class="loaderr justify-content-center" v-else>
-          <div class="loader">
-
-          </div>
+        <div class="loader"></div>
       </div>
     </div>
   </section>
@@ -47,26 +42,27 @@
 <script>
 export default {
   data() {
-    return { resumes : null,};
-
+    return { resumes: null }
   },
 
-      mounted() {
-    fetch("https://kagisomphayiportfolio.herokuapp.com/resume")
+  mounted() {
+    fetch('https://kagisomphayiportfolio.herokuapp.com/resume')
       .then((res) => res.json())
-      .then(data => {
-        console.log(data);
-        this.resumes = data;
-      });
-  },
-    };
-    
+      .then((data) => {
+        console.log(data)
+        this.resumes = data
+      })
+  }
+}
 </script>
 
 <style scoped>
 /* LOADER */
+  h5{
+        font-weight: 600;
+  }
 .loader {
-    z-index: 1000;
+  z-index: 1000;
   border: 8px solid #f3f3f3;
   border-radius: 50%;
   border-top: 8px solid rgba(115, 179, 106, 0.966);
@@ -78,26 +74,34 @@ export default {
 
 /* Safari */
 @-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
-.loaderr{
-    height: 80vh;
-    display: flex;
-    align-items: center;
+.loaderr {
+  height: 80vh;
+  display: flex;
+  align-items: center;
 }
 /* LOADER END */
 /* SLIDE */
- .slide{
+.slide {
   animation-duration: 1s;
   animation-name: slidein;
 }
-template{
+template {
   background-color: #f2f2fc;
 }
 @keyframes slidein {
@@ -112,13 +116,13 @@ template{
   }
 }
 /* SLIDE END */
-.box-shadow{
-    box-shadow: -11px 11px 13px -8px rgba(115, 179, 106, 0.966);
+.box-shadow {
+  box-shadow: -11px 11px 13px -8px rgba(115, 179, 106, 0.966);
   -webkit-box-shadow: -11px 11px 13px -8px rgba(115, 179, 106, 0.966);
   -moz-box-shadow: -11px 11px 13px -8px rgba(95, 225, 124, 0.75);
   border: none;
 }
-.green{
+.green {
   color: rgba(115, 179, 106, 0.966);
 }
 ul.timeline {
@@ -127,7 +131,7 @@ ul.timeline {
 }
 
 ul.timeline:before {
-  content: " ";
+  content: ' ';
   background: #d4d9df;
   display: inline-block;
   position: absolute;
@@ -141,12 +145,12 @@ ul.timeline > li {
   padding-left: 20px;
 }
 ul.timeline > li:before {
-  content: " ";
-  background:  rgba(115, 179, 106, 0.966);
+  content: ' ';
+  background: rgba(115, 179, 106, 0.966);
   display: inline-block;
   position: absolute;
   border-radius: 50%;
-  border: 10px solid  rgba(115, 179, 106, 0.966);
+  border: 10px solid rgba(115, 179, 106, 0.966);
   left: 20px;
   width: 20px;
   height: 20px;
@@ -156,11 +160,31 @@ ul.timeline > li:before {
   width: 90%;
   border-radius: 15px;
 }
-    @media all and (max-width: 991px) {
-    .resu {
-      width: 100%;
-      border-radius: 15px;
-    }
-    
-    }
+@media all and (max-width: 991px) {
+}
+@media all and (max-width: 767px) {
+}
+@media all and (max-width: 576px) {
+  .edu-resu{
+    font-size: 20px;
+    padding-top: 10px !important ;
+  }
+  .title{
+    font-size: 15px;
+  }
+  .period{
+    font-size: 13px;
+  }
+  h5{
+    font-size: 15px;
+    font-weight: bold;
+  }
+  .description{
+    line-height: 120%;
+  }
+}
+@media all and (max-width: 461px) {
+}
+@media all and (max-width: 330px) {
+}
 </style>
